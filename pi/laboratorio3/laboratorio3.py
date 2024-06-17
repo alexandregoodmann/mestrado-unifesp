1e7cb8752b8cd20ab3a1d5c1b60088fc91bf6c30
alexandregoodmann <alexandregoodmann@gmail.com>
2024-06-16 21:22:35 -0300

    pi - morfologia completo

--- pi/laboratorio3/laboratorio3.py
+++ pi/laboratorio3/laboratorio3.py
@@ -3,7 +3,7 @@
 # Atividade 03
 #
 # Morfologia
-# python3 laboratorio3.py '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/letra_A.jpg' 1 5 '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/letra_A_saida.jpg'
+# python3 laboratorio3.py '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/muros.png' 1 5 '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/muros_saida.png'
 # 1-erosão; 2-dilatação; 3-abertura; 4-fechamento.
 # --------------------------------------------------------------------------------------
 
@@ -19,29 +19,24 @@ os.system('cls' if os.name == 'nt' else 'clear')
 # parametros de entrada
 '''
 arquivo = sys.argv[1]
-operacao = sys.argv[2]
-tamanho = sys.argv[3]
+operacao = int(sys.argv[2])
+tamanho = int(sys.argv[3])
+arquivo_saida = sys.argv[4]
 '''
-
-arquivo = '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/letra_A.jpg'
-operacao = 2
-tamanho = 5
 elemento = []
 parte = 1
 
+arquivo = '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/muros.png'
+arquivo_saida = '/home/alexandre/projetos/mestrado-unifesp/pi/laboratorio3/muros_saida.png'
+operacao = 4
+tamanho = 5
+
 # Exibe a imagem preto e branco
 imgGray = cv2.imread(arquivo, cv2.IMREAD_GRAYSCALE)
-'''
-imgGray = imgGray[::2, ::2]
-cv2.imwrite(arquivo, imgGray)
-exit()
-'''
 (thresh, imgBW) = cv2.threshold(imgGray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-'''
 cv2.imshow('Imagem Preto e Branco', imgBW)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
 
 # --------------------------------------------------------------------------------------------
 # Cria o elemento estruturante com o tamanho de entrada
@@ -64,35 +59,36 @@ def criarElementoEstruturante():
 # i, j - coordenadas
 # d - dimensao da convolucao
 # --------------------------------------------------------------------------------------------
-def isInside(i, j):
-    segmento = imgBW[(i-parte):(i+parte+1), (j-parte):(j+parte+1)]
+def isInside(imagem, i, j):
+    segmento = imagem[(i-parte):(i+parte+1), (j-parte):(j+parte+1)]
     return np.array_equal(segmento, elemento)
 
 # --------------------------------------------------------------------------------------------
 # EROSAO
 # --------------------------------------------------------------------------------------------
-def erosao():
+def erosao(imagem):
+
     # Cria imagem branca do mesmo tamanho
-    imgErosao = np.zeros(imgGray.shape)
+    linhas, colunas = imagem.shape
+    imgErosao = np.zeros((linhas, colunas))
     imgErosao[:,:] = 255
 
     # Com base na imagem original, cria a imagem com erosao
-    linhas, colunas = imgBW.shape
     for i in range(parte, linhas-parte):
         for j in range(parte, colunas-parte):
-            if (imgBW[i,j] == 0 and isInside(i, j)):
+            if (imagem[i,j] == 0 and isInside(imagem, i, j)):
                 imgErosao[i,j] = 0
     return imgErosao
 
 # --------------------------------------------------------------------------------------------
 # DILATACAO
 # --------------------------------------------------------------------------------------------
-def dilatacao():
-    imgDilatacao = np.copy(imgBW)
-    linhas, colunas = imgBW.shape
+def dilatacao(imagem):
+    imgDilatacao = np.copy(imagem)
+    linhas, colunas = imagem.shape
     for i in range(parte, linhas-parte):
         for j in range(parte, colunas-parte):
-            if (imgBW[i,j] == 0 and isInside(i, j) == False):
+            if (imagem[i,j] == 0 and isInside(imagem, i, j) == False):
                 imgDilatacao[(i-parte):(i+parte+1), (j-parte):(j+parte+1)] = 0
     return imgDilatacao
 
@@ -109,10 +105,31 @@ elemento = criarElementoEstruturante()
 # Aplica a operacao de acordo com numero passado
 match operacao:
     case 1:
-        imgErosao = erosao()
+        imgSaida = erosao(imgBW)
     case 2:
-        imgDilatacao = dilatacao()
+        imgSaida = dilatacao(imgBW)
+    case 3:
+        imgErosao = erosao(imgBW)
+        imgSaida = dilatacao(imgErosao)
+    case 4:
+        imgDilatacao = dilatacao(imgBW)
+        imgSaida = erosao(imgDilatacao)
 
-plt.subplot(231),plt.imshow(imgBW),plt.title('ORIGINAL')
-plt.subplot(232),plt.imshow(imgDilatacao),plt.title('DILATACAO')
+# Salva a imagem processada
+cv2.imwrite(arquivo_saida, imgSaida)
+
+cv2.imshow('Imagem Saida', imgSaida)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+ero = erosao(imgBW)
+dila = dilatacao(imgBW)
+abertura = dilatacao(ero)
+fechamento = erosao(dila)
+
+plt.subplot(151),plt.imshow(imgBW, cmap='gray'),plt.title('Oritinal')
+plt.subplot(152),plt.imshow(ero, cmap='gray'),plt.title('Erosao')
+plt.subplot(153),plt.imshow(dila, cmap='gray'),plt.title('Dilatacao')
+plt.subplot(154),plt.imshow(abertura, cmap='gray'),plt.title('Abertura')
+plt.subplot(155),plt.imshow(fechamento, cmap='gray'),plt.title('Fechamento')
 plt.show()
