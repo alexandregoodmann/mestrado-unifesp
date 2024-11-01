e964c0ab3695b38f5703b7234455b550afd65422
alexandregoodmann <alexandregoodmann@gmail.com>
2024-10-31 22:58:53 -0300

    core file

--- algoritmo/aula_9_arvore_vermelho_preto/core.py
+++ algoritmo/aula_9_arvore_vermelho_preto/core.py
@@ -1,3 +1,8 @@
+import random
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
 def busca_binaria(lista, elemento):
     inicio = 0
     fim = len(lista) - 1
@@ -18,3 +23,37 @@ def busca_binaria(lista, elemento):
             fim = meio - 1
     
     return -1  # Retorna -1 se o elemento não foi encontrado
+
+def inserir2(lista, valor):
+    
+    if (valor < lista[0]):
+        lista.insert(0, valor)
+        return lista
+    
+    b = len(lista) - 1
+    if (valor > lista[b]):
+        lista.append(valor)
+        return lista
+    
+    m = b // 2
+
+    if (valor == lista[m]):
+        lista.insert(m, valor)
+        return lista
+
+    a = 0
+    b = m
+    while(b >= a):
+        print(a,b)
+        zao = len(lista[a:b]) // 2
+        if (valor < lista[zao]):
+            b = m
+        else:
+            a = m
+        #print(lista[a:b])
+
+    return lista
+
+vet = [0, 1, 3, 3, 4, 5, 6, 6, 7, 7, 10]
+ret = inserir2(vet, 2)
+print(ret)
