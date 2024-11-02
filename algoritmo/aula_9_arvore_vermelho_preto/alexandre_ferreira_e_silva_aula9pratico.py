da51b58fdf105e668f6fbfb5295e1923bfb51c80
alexandregoodmann <alexandregoodmann@gmail.com>
2024-11-02 09:58:37 -0300

    aula9 vetor ordenado

--- algoritmo/aula_9_arvore_vermelho_preto/alexandre_ferreira_e_silva_aula9pratico.py
+++ algoritmo/aula_9_arvore_vermelho_preto/alexandre_ferreira_e_silva_aula9pratico.py
@@ -5,48 +5,72 @@
 # Aluno: Alexandre Ferreira e Silva
 # Atividade Aula 9
 # Fonte: ChatGPT, Google Gemini
+# Obs.: Caso ocorra exception ao buscar os números 50 e 50000, 
+#       rodar o algorítmo algumas vezes pois os números são gerados randomicamente
 # --------------------------------------------------------------------------------------
 import os
 import time
 import random
 from tabulate import tabulate # >>>>>> pip install tabulate 
-from core import busca_binaria
+from core import inserir, pesquisar
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # ------------------------------------------------------------------------------------------------------
-# vetores aleatorios
+# VETOR ALEATORIO
 # ------------------------------------------------------------------------------------------------------
 # -- inserção -- 
 start_time = time.time()
-numeros_aleatorios = [random.randint(0, 10) for _ in range(10)]
+numeros_aleatorios = [random.randint(0, 100000) for _ in range(100000)]
 tempo_insercao_aleatorio =  {time.time() - start_time}
 
 # -- pesquisa 50 --
 start_time = time.time()
 for i in (numeros_aleatorios):
     if (i == 50):
-        break;
+        break
 tempo_50_aleatorio =  {time.time() - start_time}
 
 # -- pesquisa 50000 --
 start_time = time.time()
 for i in (numeros_aleatorios):
     if (i == 50000):
-        break;
+        break
 tempo_50mil_aleatorio =  {time.time() - start_time}
 
-vet_valor_aleatorio = ['Valor aleatorio', tempo_insercao_aleatorio, tempo_50_aleatorio, tempo_50mil_aleatorio]
+vet_valor_aleatorio = ['Vetor aleatorio', tempo_insercao_aleatorio, tempo_50_aleatorio, tempo_50mil_aleatorio]
+
 # ------------------------------------------------------------------------------------------------------
-lista_ordenada = []
-lista_ordenada.append(numeros_aleatorios[0])
+# VETOR ORDENADO
+# ------------------------------------------------------------------------------------------------------
+# -- inserção -- 
+vetor_ordenado = []
+start_time = time.time()
+for item in numeros_aleatorios:
+    inserir(vetor_ordenado, item)
+tempo_insert_ordenado =  {time.time() - start_time}
 
-print(numeros_aleatorios, lista_ordenada)
+# -- pesquisa 50 --
+start_time = time.time()
+index = pesquisar(vetor_ordenado, 50)
+if (index == -1):
+    raise Exception('Nao encontrou 50')
+tempo_50_ordenado =  {time.time() - start_time}
 
+# -- pesquisa 50000 --
+start_time = time.time()
+index = pesquisar(vetor_ordenado, 50000)
+if (index == -1):
+    raise Exception('Nao encontrou 50000')
+tempo_50mil_ordenado =  {time.time() - start_time}
 
+vet_valor_ordenado = ['Vetor ordenado', tempo_insert_ordenado, tempo_50_ordenado, tempo_50mil_ordenado]
+# ------------------------------------------------------------------------------------------------------
+# RESULTADOS
+# ------------------------------------------------------------------------------------------------------
 table = [
             ['', 'Tempo insercao', 'Pesquisa 50', 'Pesquisa 50.000'],
-            vet_valor_aleatorio
+            vet_valor_aleatorio,
+            vet_valor_ordenado
         ]
-
 print(tabulate(table))
--- algoritmo/aula_9_arvore_vermelho_preto/core.py
+++ algoritmo/aula_9_arvore_vermelho_preto/core.py
@@ -3,57 +3,61 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-def busca_binaria(lista, elemento):
+def pesquisar(lista, elemento):
     inicio = 0
     fim = len(lista) - 1
     
     while inicio <= fim:
         meio = (inicio + fim) // 2
         
-        # Verifica se o elemento está no meio
         if lista[meio] == elemento:
             return meio  # Retorna o índice do elemento
         
-        # Se o elemento é maior, ignora a metade esquerda
         elif lista[meio] < elemento:
             inicio = meio + 1
             
-        # Se o elemento é menor, ignora a metade direita
         else:
             fim = meio - 1
     
-    return -1  # Retorna -1 se o elemento não foi encontrado
+    return -1
 
-def inserir2(lista, valor):
+def inserir(lista, valor):
+
+    if len(lista) == 0:
+        lista.append(valor)
+        return
     
     if (valor < lista[0]):
         lista.insert(0, valor)
-        return lista
+        return 
     
+    a = 0
     b = len(lista) - 1
     if (valor > lista[b]):
         lista.append(valor)
-        return lista
+        return 
     
     m = b // 2
 
     if (valor == lista[m]):
         lista.insert(m, valor)
-        return lista
+        return 
 
-    a = 0
-    b = m
-    while(b >= a):
-        print(a,b)
-        zao = len(lista[a:b]) // 2
-        if (valor < lista[zao]):
+    while(b - a > 1):
+        if (valor == lista[m]):
+            lista.insert(m, valor)
+            return 
+        if (valor < lista[m]):
+            # esquerdo
             b = m
+            m = b // 2
         else:
+            #direito
             a = m
-        #print(lista[a:b])
-
-    return lista
-
-vet = [0, 1, 3, 3, 4, 5, 6, 6, 7, 7, 10]
-ret = inserir2(vet, 2)
+            m = (a + b) // 2
+    lista.insert(b, valor)
+'''
+vet = [0, 1, 3, 3, 4, 5, 6, 6, 7, 7, 10, 13, 27, 33, 41, 45, 46, 50, 58, 59, 60]
+ret = inserir(vet, 30)
 print(ret)
+'''
