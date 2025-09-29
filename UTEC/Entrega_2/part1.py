edcf5b80123e5bd816e356e2b12133723706c15b
alexandregoodmann <alexandregoodmann@gmail.com>
2025-09-29 16:32:55 -0300

    lista 2 utec

---
+++ UTEC/Entrega_2/part1.py
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------------
+# Parte 1 — Conceitos Básicos (Classe e Objeto)
+# --------------------------------------------------------------------------------
+
+# Ex 1 — Retângulo: classe Retangulo com largura, altura, métodos area() e perimetro().
+class Retangulo:
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
+
+    def area(self):
+        return self.largura * self.altura
+    
+    def perimetro(self):
+        return 2*(self.largura + self.altura)
+    
+ret = Retangulo(2, 3)
+print('Area do retangulo: ', ret.area())
+print('Perimetro do retangulo: ', ret.perimetro())
+
+# Ex 2 — Conta Bancária: classe Conta com titular, saldo, métodos depositar() e sacar() (validar saldo).
+class ContaBancaria:
+    saldo = 0
+    def __init__(self, titular):
+        self.titular = titular
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    
