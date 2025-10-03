ffbeb91d00b593b90fa48e6cbf86ff46fc0a93a0
alexandregoodmann <alexandregoodmann@gmail.com>
2025-10-03 12:59:15 -0300

    robo 3d

--- UTEC/Entrega_2/robo_manipulador.py
+++ UTEC/Entrega_2/robo_manipulador.py
@@ -14,6 +14,7 @@
 import math
 import matplotlib.pyplot as plt
 import os
+import plotly.graph_objects as go
 
 #limpar console
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -39,20 +40,18 @@ class Componente2D:
     def __str__(self):
         return f"{self.angulo, self.x_inicial, self.y_inicial, self.x_final, self.y_final}"
     
-class Componente3D(Componente2D):
-    z_inicial = 0
-    z_final = 0
-    z_deslocamento = 0
-
-    def __init__(self, nome, angulo=0, comprimento=5, limite_min=-180, limite_max=180):
+class Componente3D():
+    x = 0
+    y = 0
+    z = 0
+    def __init__(self, nome, theta=0, gama=0, comprimento=5):
         self.nome = nome
-        self.angulo = angulo
+        self.theta = theta
+        self.gama = gama
         self.comprimento = comprimento
-        self.limite_min = limite_min
-        self.limite_max = limite_max
 
     def __str__(self):
-        return f"{self.angulo, self.x_inicial, self.y_inicial, self.x_final, self.y_final}"
+        return f"{self.theta, self.gama, self.x, self.y, self.z}"
 
 # --------------------------------------------------------------------------------------
 # Classe para manipular um robô 2D com demonstração gráfica
@@ -102,6 +101,55 @@ class Manipulador2D:
         plt.gca().set_aspect('equal', adjustable='box') # Garante que a escala seja igual para os eixos
         plt.show()
 
+class Manipulador3D():
+    componentes = []
+    def __init__(self):
+        pass
+
+    def addComponent(self, componente):
+        tamanho = self.componentes.__len__()
+        if (tamanho == 0):
+            gama = componente.gama
+            theta = componente.theta
+            h = componente.comprimento * math.cos(math.radians(componente.gama))
+            x = h * math.cos(math.radians(componente.theta))
+            y = h * math.sin(math.radians(componente.theta))
+            z = componente.comprimento * math.sin(math.radians(componente.gama))
+        else:
+            anterior = self.componentes[tamanho - 1]
+            gama = componente.gama + anterior.gama
+            theta = componente.theta + anterior.theta
+            h = componente.comprimento * math.cos(math.radians(gama))
+            x = anterior.x + h * math.cos(math.radians(theta))
+            y = anterior.y + h * math.sin(math.radians(theta))
+            z = anterior.z + componente.comprimento * math.sin(math.radians(gama))
+        
+        componente.gama = gama
+        componente.theta = theta
+        componente.x = x
+        componente.y = y
+        componente.z = z
+        print('Componente 3D: ', componente)
+        self.componentes.append(componente)
+
+    def exibirGrafico3D(self):
+        fig = go.Figure()
+
+        for index, item in enumerate(self.componentes):
+            if index == 0:
+                fig.add_trace(go.Scatter3d(x=[0, item.x], y=[0, item.y], z=[0, item.z], mode="lines+markers", line=dict(width=6, color="blue"), marker=dict(size=4, color="red")))
+            else:
+                anterior = self.componentes[index - 1]
+                fig.add_trace(go.Scatter3d(x=[anterior.x, item.x], y=[anterior.y, item.y], z=[anterior.z, item.z], mode="lines+markers", line=dict(width=6, color="blue"), marker=dict(size=4, color="red")))
+
+        fig.update_layout(scene=dict(
+            xaxis=dict(range=[0, 60]),
+            yaxis=dict(range=[0, 60]),
+            zaxis=dict(range=[0, 60])),
+            title="Exemplo de Vetor 3D com Plotly"
+        )
+        fig.show()
+
 j1 = Componente2D('J1', 30, 30)
 j2 = Componente2D('J2', 30, 20)
 j3 = Componente2D('J3', -30, 12)
@@ -111,11 +159,14 @@ manipulador2D.addComponente(j1)
 manipulador2D.addComponente(j2)
 manipulador2D.addComponente(j3)
 print('Posição Final: ', j3.x_final, j3.y_final)
-manipulador2D.exibir_grafico2D()
+#manipulador2D.exibir_grafico2D()   
 
-class Manipulador3D(Manipulador2D):
-    def __init__(self):
-        super().__init__()
-        self.
+j3d_1 = Componente3D('J3D1', 45, 45, 30)
+j3d_2 = Componente3D('J3D2', 0, -45, 20)
+j3d_3 = Componente3D('J3D3', 0, 45, 10)
 
-    
+manipulador3D = Manipulador3D()
+manipulador3D.addComponent(j3d_1)
+manipulador3D.addComponent(j3d_2)
+manipulador3D.addComponent(j3d_3)
+manipulador3D.exibirGrafico3D()
